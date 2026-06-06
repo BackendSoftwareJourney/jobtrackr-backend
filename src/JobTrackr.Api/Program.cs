eff9c2a9c9fe08e61fe7686e8cd472cb28ce8ef9
BackendSoftwareJourney <BackendSoftwareJourney@users.noreply.github.com>
2026-06-06 10:33:24 +0500

    Day 24: Register SQL Server DbContext

--- src/JobTrackr.Api/Program.cs
+++ src/JobTrackr.Api/Program.cs
@@ -1,5 +1,7 @@
 using JobTrackr.Application.Tasks;
 using JobTrackr.Application.Users;
+using JobTrackr.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,8 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddSingleton<IUserService, UserService>();
 builder.Services.AddSingleton<ITaskService, TaskService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
